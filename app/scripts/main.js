ee5015c2432e7f5506327b31eb3baf537f584ac4
Duarte Aragao <daragao@gmail.com>
2014-04-11 18:14:10 +0100

    changed search box

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -65,12 +65,12 @@ require([
             $.ajaxPrefilter("json script", function (options, originalOptions, jqXHR) {
                 // GETEON BACKEND
                 //var urlBaseGeteonBackend = 'http://geteon-backend-php.herokuapp.com/v1'; //remote
-                //var urlBaseGeteonBackend = 'http://api.geteon.com/v1'; //remote
-                var urlBaseGeteonBackend = 'http://localhost/newsline1'; //local
+                var urlBaseGeteonBackend = 'http://api.geteon.com/v1'; //remote
+                //var urlBaseGeteonBackend = 'http://localhost/newsline1'; //local
                 // go BACKEND
                 //var urlBaseGoBackend = 'http://warm-brook-6217.herokuapp.com/api/v1'; //remote
-                //var urlBaseGoBackend = 'http://apiuser.geteon.com/api/v1'; //local
-                var urlBaseGoBackend = 'http://localhost:4000/api/v1'; //local
+                var urlBaseGoBackend = 'http://apiuser.geteon.com/api/v1'; //local
+                //var urlBaseGoBackend = 'http://localhost:4000/api/v1'; //local
                 var backendKeyword = '{GO-BACKEND}';
             var fullAddress = 'http://';
             var keywordIndex = options.url.indexOf(backendKeyword);
